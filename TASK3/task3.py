3aa5a4b7ba08f4f81f63da2f011517f365dbd229
Zainab Sana <zainabwork11@gmail.com>
2026-07-24 11:50:05 +0500

    Added task 3

---
+++ TASK3/task3.py
@@ -0,0 +1,62 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+#==========================GUASSIAN FILTER========================================
+# Read grayscale image
+image = cv2.imread("../TASK1/image.jpg", cv2.IMREAD_GRAYSCALE)
+
+if image is None:
+    print("Image not found!")
+    exit()
+
+#get image dimensions
+height,width=image.shape
+
+#create an output image
+output=np.zeros((height,width),dtype=np.uint8)
+
+
+#Define Guassian filter
+kernel=[
+    [1,2,1],
+    [2,4,2],
+    [1,2,1]
+]
+
+# OUTER LOOP: Move kernel over image
+for row in range(1, height - 1):
+    for col in range(1, width - 1):
+        total = 0
+        # Visit all 9 pixels inside the kernel
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                pixel = int(image[row + i][col + j])
+                kernel_value = kernel[i + 1][j + 1]
+                total = total + ((pixel * kernel_value))
+
+        total=total//16
+        output[row][col] = int(total)
+
+# Display images
+plt.figure(figsize=(10, 5))
+
+plt.subplot(1, 2, 1)
+plt.imshow(image, cmap="gray")
+plt.title("Original Image")
+plt.axis("off")
+
+plt.subplot(1, 2, 2)
+plt.imshow(output, cmap="gray")
+plt.title("After applying guassian filter")
+plt.axis("off")
+
+
+
+success = cv2.imwrite("guassian_filter_image.jpg", output)
+
+plt.show()
+
+if success:
+    print("Image saved successfully!")
+else:
+    print("Failed to save image.")
